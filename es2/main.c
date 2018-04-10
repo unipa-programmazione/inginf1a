cc15af5a1b25518a04f64d1f6b1c7e99f1671857
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2018-04-10 17:26:12 +0200

    fix code

--- es2/main.c
+++ es2/main.c
@@ -1,40 +1,34 @@
 #include <stdio.h>
 
-int main (){
-  int intero;
+int main() {
+    int intero;
+    int i=2; //contatore
+    int primo=1; 
   
-  puts("Inserisci un numero intero");
-  scanf("%d", &intero);
+    puts("Inserisci un numero intero");
+    scanf("%d", &intero);
   
-  if (intero < 2){
-    puts("Numero non valido");
-    return -1;
-  }
+    if (intero < 2){
+        puts("Numero non valido");
+        primo = 0;
+        return -1;
+    }
   
-  int i=2; //contatore
-  int primo=1; 
-  
-  while (i<primo){
-    if (primo%i == 0){
-      puts("Il numero non è primo");
-      break;
-    } else
-      i++;
-    } //fine ciclo
-  if (primo == 1){
-    puts("Il numero è primo");
-  }else{
-    puts("Il numero non è primo");
-  }
-}
+    while (i < intero){
+        if (intero%i == 0){
+            primo = 0;
+            break;
+        }
+        else {
+            i++;
+        } //fine ciclo
+    }
 
-/*main:
-  
-  primo = vero
-  finche condizione
-    se numero%i == 0
-      primo = falso
-      esci
-    altrimenti
-      i = i+1
-*/
+    if (primo == 1){
+        puts("Il numero è primo");
+    }
+
+    else {
+        puts("Il numero non è primo");
+    }
+}
